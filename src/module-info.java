f2253d759e00c0e6e1224ca70f7c783a5c7186fd
dishonjp <dishonjp11@gmail.com>
2020-01-03 10:19:13 +0530

    [yaminijyothi] add.leapyear

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+class  leapyear {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in)
+				System.out.println("enter a year");
+		int n=sc.nextInt();
+		if(n%4==0)
+		{
+			System.out.println("leap year");
+		}
+		else if
+		{
+		  System.out.println("not a leap year");
+		}
+		else
+		{
+			System.out.println("invalid data");
+		}
+		 
+	}
+}
